a7f0e8ebfe6659ef793bb296740e87308a3a9ac3
Monjur Alahi Bizu <bizutechbd@gmail.com>
2023-02-18 17:18:18 +0600

    Work start

--- problem-1.js
+++ problem-1.js
@@ -1,4 +1,4 @@
-function mindGame(num) {
+/* function mindGame(num) {
     if (!num || num < 0);
   
     const result = (num * 3 + 10) / 2 - 5;
@@ -6,4 +6,52 @@ function mindGame(num) {
     return result;
   }
 
-  console.log(mindGame(5));
+  console.log(mindGame(5)); */
+
+  /* function compare(a, b) {
+
+    if (a == b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(15, "15");
+   
+   console.log(result); */
+
+ /*   function compare(a, b) {
+
+    if (a.toString() === b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(25, 25);
+   
+   console.log(result);
+
+   console.log("123" + 123);
+
+let a='hi';
+
+let b='there';
+
+console.log(a+b); */
+
+
+for( let i = 0; i <=100; i++);
+console.log(i);
